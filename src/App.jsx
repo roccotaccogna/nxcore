cc48e2f6559d775860c460245805eb951299b7ef
Rocco Taccogna <roccotaccogna@hotmail.com>
2026-05-12 11:33:29 +0200

    update + vercel

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-x-hidden">
       <Navbar />
       <Hero />
       <TrustBanner />
